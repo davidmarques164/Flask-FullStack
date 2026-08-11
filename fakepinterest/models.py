e8641b8b052df5d792ea30a89853c7f12e352e59
davidmarques164 <davidpeterpaulmarques@gmail.com>
2026-08-11 15:09:26 -0300

    feat: adicionar modelos de Usuario e Foto ao banco de dados

---
+++ fakepinterest/models.py
@@ -0,0 +1,17 @@
+from fakepinterest import database
+from datetime import datetime, timezone 
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(20), unique=True, nullable=False)
+    email = database.Column(database.String(120), unique=True, nullable=False)
+    senha = database.Column(database.String(60), nullable=False)
+    fotos = database.relationship('Foto', backref='autor', lazy=True)
+class Foto(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    imagem = database.Column(database.String(20), default='default.png', nullable=False)
+    data_criacao = database.Column(
+        database.DateTime,
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
