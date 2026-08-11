d6531adadf0baf02ad313d13456ca38251f034bb
davidmarques164 <davidpeterpaulmarques@gmail.com>
2026-08-11 19:25:35 -0300

    feat: adicionar autenticação de usuário e estrutura de banco de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6'
+
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from fakepinterest import routes
--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,17 +1,26 @@
-from fakepinterest import database
-from datetime import datetime, timezone 
-class Usuario(database.Model):
+from fakepinterest import database, login_manager
+from datetime import datetime, timezone
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(id_usuario): 
+    return Usuario.query.get(int(id_usuario))
+class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(20), unique=True, nullable=False)
     email = database.Column(database.String(120), unique=True, nullable=False)
     senha = database.Column(database.String(60), nullable=False)
-    fotos = database.relationship('Foto', backref='autor', lazy=True)
+    fotos = database.relationship("Foto", backref="usuario", lazy=True)
+
+
 class Foto(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    imagem = database.Column(database.String(20), default='default.png', nullable=False)
+    imagem = database.Column(database.String(20), default="default.png", nullable=False)
     data_criacao = database.Column(
-        database.DateTime,
-        nullable=False,
+        database.DateTime, 
+        nullable=False, 
         default=lambda: datetime.now(timezone.utc)
     )
-    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
+    id_usuario = database.Column(
+        database.Integer, database.ForeignKey("usuario.id"), nullable=False
+    )
